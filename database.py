6c89303608d0f327b2e77d7b7f979c470a07531f
Japhet Masuama <japhetmasuamamn@gmail.com>
2026-07-03 08:45:14 -0700

    Restauration de get_db

--- database.py
+++ database.py
@@ -19,3 +19,11 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+# Dépendance pour obtenir la DB dans les routes
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
